19b1557407aecd2e625b1f2c31ddbf632a3368e0
RazorBest <razzvy_20@yahoo.com>
2019-06-05 06:53:55 +0300

    Setup and documentation

--- Multiple Pendulum Simulation in Python/setup.py
+++ Multiple Pendulum Simulation in Python/setup.py
@@ -1,4 +1,14 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['main.py'])
+setup(windows=['main.py'],
+        options={
+            'py2exe': {
+                "dll_excludes": 
+                ['api-ms-win-core-string-l1-1-0.dll',
+                 'api-ms-win-core-psapi-l1-1-0.dll',
+                 'api-ms-win-core-registry-l1-1-0.dll',
+                 'api-ms-win-core-localization-l1-2-0.dll'
+                 'libopenblas.TXA6YQSD3GCQQC22GEQ54J2UDCXDXHWN.gfortran-win_amd64.dll']
+            }
+        })
